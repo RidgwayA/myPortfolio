ee3ca00b4cdecb98a2321a67a2351e5504e2bdd3
RidgwayA <52265069+RidgwayA@users.noreply.github.com>
2025-08-04 22:58:04 -0600

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Navbar from './components/Navbar';
+import { Analytics } from '@vercel/analytics/next';
 
 const App = () => {
 
   return (
     <div className='font-body'>
+      <Analytics/>
       <Landing />
       <Navbar />
       <About />
